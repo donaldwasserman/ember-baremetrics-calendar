4a4a68ee6d1c5997022e2c41d89ec094f82b47cd
Donald Wasserman <djwasserman@gmail.com>
2017-09-07 23:37:23 -0400

    upgrades and de-bowering

--- index.js
+++ index.js
@@ -14,20 +14,26 @@ module.exports = {
     var options = app.options.baremetricsCalendar || {};
 
     var isFastBoot = process.env.EMBER_CLI_FASTBOOT === 'true';
+
     if (!isFastBoot) {
-      this.import('vendor/Calendar.js')
+      app.import('vendor/Calendar.js')
     }
+
     if (options.includeStyles !== false) {
-      this.import('vendor/calendar.css');
+      app.import('vendor/calendar.css');
     }
   },
-  afterInstall() {
-    return this.addPackageToProject('BaremetricsCalendar');
-  },
+
   treeForVendor(vendorTree) {
-    var BareMetricsTree = new Funnel(path.join(this.project.root, 'node_modules', 'BaremetricsCalendar'), {
-      files: ['public/js/Calendar.js', 'public/css/application.js']
+    console.log(path.dirname(this.project.root, 'node_modules', 'BaremetricsCalendar', 'public'));
+    let BareMetricsTree = new Funnel(path.join(this.project.root, 'node_modules', 'BaremetricsCalendar', 'public'), {
+      files: ['js/Calendar.js']
     });
+
+    let trees = [BareMetricsTree];
+    if (vendorTree) {
+      trees.push(vendorTree);
+    }
 
     return new MergeTrees([vendorTree, BareMetricsTree]);
   }
