32451066e563884b36a7572255578152d412d7ad
Donald Wasserman <djwasserman@gmail.com>
2017-09-07 22:19:47 -0400

    halfway making changes

--- blueprints/ember-baremetrics-calendar/index.js
+++
@@ -1,8 +0,0 @@
-/*jshint node:true*/
-module.exports = {
-  description: 'Add baremetrics-calendar dependencies',
-  normalizeEntityName: function() {},
-  afterInstall: function() {
-    return this.addBowerPackageToProject('BaremetricsCalendar');
-  }
-};
--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path'),
+    Funnel = require('broccoli-funnel'),
+    MergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
   name: 'ember-baremetrics-calendar',
 
-  included: function(app) {
+  included(app) {
     this._super.included.apply(this, arguments);
-
-    // see: https://github.com/ember-cli/ember-cli/issues/3718
-    while (typeof app.import !== 'function' && app.app) {
-      app = app.app;
-    }
 
     var options = app.options.baremetricsCalendar || {};
 
     var isFastBoot = process.env.EMBER_CLI_FASTBOOT === 'true';
     if (!isFastBoot) {
-      app.import('bower_components/BaremetricsCalendar/public/js/Calendar.js');
+      this.import('vendor/Calendar.js')
     }
     if (options.includeStyles !== false) {
-      app.import('bower_components/BaremetricsCalendar/public/css/application.css');
+      this.import('vendor/calendar.css');
     }
+  },
+  afterInstall() {
+    return this.addPackageToProject('BaremetricsCalendar');
+  },
+  treeForVendor(vendorTree) {
+    var BareMetricsTree = new Funnel(path.join(this.project.root, 'node_modules', 'BaremetricsCalendar'), {
+      files: ['public/js/Calendar.js', 'public/css/application.js']
+    });
 
-    return app;
+    return new MergeTrees([vendorTree, BareMetricsTree]);
   }
-
 };
